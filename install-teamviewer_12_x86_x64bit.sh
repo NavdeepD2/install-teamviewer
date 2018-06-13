160e66f3eb02cb952c9654e7239a858d90881e15
Navdeep Singh <navdeep.d2@gmail.com>
2018-06-13 17:53:55 +0530

    Update install-teamviewer_12_x86_x64bit.sh

--- install-teamviewer_12_x86_x64bit.sh
+++ install-teamviewer_12_x86_x64bit.sh
@@ -9,12 +9,13 @@ dpkg -r teamviewer:i386
 dpkg --purge teamviewer
 dpkg --purge teamviewer:i386
 apt autoremove -y
+rm -fr /tmp/teamviewer*
 #cd /tmp/ ; wget http://download.teamviewer.com/download/version_12x/teamviewer_amd64.deb
 cd /tmp/ ; wget http://download.teamviewer.com/download/version_12x/teamviewer_i386.deb
 dpkg -i /tmp/teamviewer_i386.deb
 apt-get install libgcc1 -y
 apt-get -f install -y
-dpkg -l | grep teamv
+dpkg -l | grep -i teamviewer
 if [[ $? != 0 ]]; then
 echo "============================================"
 echo -e "\e[1;31m TEAM VIEWER 12 INSTALLATION FAILED \e[0m"
