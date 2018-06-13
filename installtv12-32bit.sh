44bedba2b5fc4327b9ff4751f477fa6084a034ff
Navdeep Singh <navdeep.d2@gmail.com>
2018-06-13 17:45:33 +0530

    Add files via upload

---
+++ installtv12-32bit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "============================================"
+echo -e "\e[1;32m REMOVING PREVIOUS TEAMVIEWER IF ANY ............ \e[0m"
+echo "============================================"
+apt-get remove teamviewer -y
+apt autoremove -y
+apt-get remove teamviewer:i386 -y
+dpkg -r teamviewer:i386
+dpkg --purge teamviewer
+dpkg --purge teamviewer:i386
+apt autoremove -y
+#cd /tmp/ ; wget http://download.teamviewer.com/download/version_12x/teamviewer_amd64.deb
+cd /tmp/ ; wget http://download.teamviewer.com/download/version_12x/teamviewer_i386.deb
+dpkg -i /tmp/teamviewer_i386.deb
+apt-get install libgcc1 -y
+apt-get -f install -y
+dpkg -l | grep teamv
+if [[ $? != 0 ]]; then
+echo "============================================"
+echo -e "\e[1;31m TEAM VIEWER 12 INSTALLATION FAILED \e[0m"
+echo "============================================"
+else
+echo "============================================"
+echo -e "\e[1;32m TEAM VIEWER 12 INSTALLED SUCCESSFULLY \e[0m"
+echo "============================================"
+fi
